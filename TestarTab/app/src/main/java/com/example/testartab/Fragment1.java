d12d140b69fb414a6655db855e0e66575366eda2
Larisa-luz <larisa.castellanos@gmail.com>
2021-11-22 20:33:01 +0100

    Lab1 med TabLayout och flik2

---
+++ TestarTab/app/src/main/java/com/example/testartab/Fragment1.java
@@ -0,0 +1,27 @@
+package com.example.testartab;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1_layout, container, false);
+    }
+
+
+
+
+}
+
